3a6274cd547bf05edb4b4637825c790dbe1dc2c1
zhangchun <chunzai@xiaoyubobo.com>
2021-02-25 18:11:52 +0800

    add:新增加一个类

---
+++ grammer/src/main/java/com/jimmy/kotlinstudy/SixActivity.kt
@@ -0,0 +1,12 @@
+package com.jimmy.kotlinstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SixActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_six)
+    }
+}
