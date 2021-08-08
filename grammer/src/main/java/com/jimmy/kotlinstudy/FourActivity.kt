af3065562821b0d5d5005a6d525dc9f67a61ee37
zhangchun <13795497151@163.com>
2021-08-08 19:05:46 +0800

    新增README文件说明

--- grammer/src/main/java/com/jimmy/kotlinstudy/FourActivity.kt
+++ grammer/src/main/java/com/jimmy/kotlinstudy/FourActivity.kt
@@ -1,7 +1,7 @@
 package com.jimmy.kotlinstudy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.jimmy.kotlinstudy.utils.DateUtil
 import kotlinx.android.synthetic.main.activity_four.*
 import java.text.SimpleDateFormat
